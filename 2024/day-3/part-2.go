c680b8b156e48b87d3802a9595afe959ef6fb778
Rafał Berezin <rafalberezin@protonmail.com>
2024-12-03 06:45:48 +0100

    feat(2024): solve day 3 part 2

---
+++ 2024/day-3/part-2.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/RafalBerezin/advent-of-code/2024/lib"
+)
+
+func Part2() {
+	ql := lib.NewQuickLogger(3, 2)
+	ql.Title()
+
+	input, err := lib.LoadInputFile(3).Strings()
+	lib.CheckError(err)
+
+	matcher := regexp.MustCompile(`mul\(\d{1,3},\d{1,3}\)|do(n't)?\(\)`)
+
+	enabled := true
+	var result int
+
+	for _, line := range input {
+		matches := matcher.FindAll([]byte(line), -1)
+
+		for _, match := range matches {
+			if match[0] == byte('d') {
+				enabled = len(match) == 4
+				continue
+			}
+
+			if !enabled {
+				continue
+			}
+
+			nums := strings.Split(string(match[4:len(match)-1]), ",")
+
+			a, err := strconv.Atoi(nums[0])
+			lib.CheckError(err)
+			b, err := strconv.Atoi(nums[1])
+			lib.CheckError(err)
+
+			result += a * b
+		}
+	}
+
+	ql.Solve(result)
+}
